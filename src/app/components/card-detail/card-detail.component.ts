75e24ce914c46fe74d826efba3cbcfca1a53c55b
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-06-10 00:57:32 +0900

    card detail components

---
+++ src/app/components/card-detail/card-detail.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-detail',
+  templateUrl: './card-detail.component.html',
+  styleUrls: ['./card-detail.component.css']
+})
+export class CardDetailComponent {}
