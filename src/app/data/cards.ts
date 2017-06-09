c8ea64620139af2f577bf6a73edb11a233693eee
Sheenan Tenepre <sheenantenepre@gmail.com>
2017-06-10 00:58:05 +0900

    cards

---
+++ src/app/data/cards.ts
@@ -0,0 +1,10 @@
+export const CARDS = [
+  { id: 0, value: 0, display: '0' },
+  { id: 50, value: 0.5, display: '0.5' },
+  { id: 1, value: 1, display: '1' },
+  { id: 2, value: 2, display: '2' },
+  { id: 3, value: 3, display: '3' },
+  { id: 5, value: 5, display: '5' },
+  { id: 8, value: 8, display: '8' },
+  { id: 99, value: 99, display: '∞' },
+];
